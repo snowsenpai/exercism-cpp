234c131e26a1d29026f03a41dbe1301719a231c9
Umoh Samuel <samuelwisdom360@gmail.com>
2026-08-14 08:50:12 +0100

    Complete log levels exercise on string formatting

--- log-levels/log_levels.cpp
+++ log-levels/log_levels.cpp
@@ -3,13 +3,19 @@
 namespace log_line {
 std::string message(std::string line) {
     // return the message
+    return line.substr(line.find(" ") + 1);
 }
 
 std::string log_level(std::string line) {
     // return the log level
+    return line.substr(1, line.find("]"));
 }
 
 std::string reformat(std::string line) {
     // return the reformatted message
+    std::string message = line.substr(line.find(" ") + 1);
+    std::string level = line.substr(1, line.find("]"));
+
+    return message + " (" + level + ")";
 }
 }  // namespace log_line
