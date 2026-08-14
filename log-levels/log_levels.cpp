2e656a6fd3dc45cc9503e5a3debfdf21d3668c99
Umoh Samuel <samuelwisdom360@gmail.com>
2026-08-14 16:45:08 +0100

    fixed bug in log_line::log_level and log_line::reformat

--- log-levels/log_levels.cpp
+++ log-levels/log_levels.cpp
@@ -8,13 +8,13 @@ std::string message(std::string line) {
 
 std::string log_level(std::string line) {
     // return the log level
-    return line.substr(1, line.find("]"));
+    return line.substr(1, line.find("]") - 1);
 }
 
 std::string reformat(std::string line) {
     // return the reformatted message
     std::string message = line.substr(line.find(" ") + 1);
-    std::string level = line.substr(1, line.find("]"));
+    std::string level = line.substr(1, line.find("]") - 1);
 
     return message + " (" + level + ")";
 }
